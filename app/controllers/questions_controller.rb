6ebed61121cf4af57e6d6faad031e270f0080b20
Agnès de Briey <debrieyagnes@gmail.com>
2020-11-09 16:10:00 +0100

    Ex 2 - Stupid Coaching v2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,10 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    question = params[:my_question]
-    if question == "I am going to work"
-      @coach_answer = "great"
-    elsif question[-1] == "?"
+    @question = params[:my_question]
+    if @question == "I am going to work"
+      @coach_answer = "Great"
+    elsif @question[-1] == "?"
       @coach_answer = "Silly question, get dressed and go to work!"
     else
       @coach_answer = "I don't care, get dressed and go to work!"
